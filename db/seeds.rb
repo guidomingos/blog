6a35a62390680eabc2897f328a7c21f7201ce16a
Guilherme <guilherme63@gmail.com>
2023-11-09 10:15:22 -0300

    Devise + Tailwindcss

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.where(email: "guilherme63@gmail.com").first_or_initialize
+user.update!(
+    password: "qwe123",
+    password_confirmation: "qwe123"
+)
